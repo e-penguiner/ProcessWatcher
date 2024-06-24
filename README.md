d0d95242c65a9fb61554316dc87e38b2944f3c99
e-penguiner <142122505+e-penguiner@users.noreply.github.com>
2024-06-24 13:10:32 +0900

    support multiprocess

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
-# Process Monitoring Tool
+# Process-Watcher
 
-This Rust application monitors the start and stop events of a specific process on a Windows system. It uses Windows Management Instrumentation (WMI) to receive notifications for process events and logs relevant information.
+This Rust application monitors the start and stop events of specific processes on a Windows system. It uses Windows Management Instrumentation (WMI) to receive notifications for process events and logs relevant information.
 
 ## Features
 
 - Monitors process start and stop events.
 - Logs process ID, name, start time, and stop time.
 - Continuously prints current process information every 5 seconds.
+- Supports monitoring multiple processes specified in a configuration file.
 
 ## Requirements
 
@@ -19,8 +20,8 @@ This Rust application monitors the start and stop events of a specific process o
 1. **Clone the repository:**
 
    ```bash
-   git clone https://github.com/yourusername/process-monitoring-tool.git
-   cd process-monitoring-tool
+   git clone https://github.com/yourusername/process-watcher.git
+   cd process-watcher
    ```
 
 2. **Build the project:**
@@ -29,27 +30,33 @@ This Rust application monitors the start and stop events of a specific process o
    cargo build --release
    ```
 
-3. **Run the project:**
+3. **Prepare the configuration file:**
+
+   Create a `config.yaml` file in the root directory of the project with the following structure:
+
+   ```yaml
+   watch_list:
+     - "paraview.exe"
+     - "Code.exe"
+   ```
+
+   Modify the `watch_list` to include the processes you want to monitor.
+
+4. **Run the project:**
 
    ```bash
    cargo run --release
    ```
 
-4. **Customize the process name:**
-
-   By default, the tool monitors `Notepad.exe`. To monitor a different process, modify the `process_name` variable in the `main` function.
-
-   ```rust
-   let process_name = Arc::new("YourProcessName.exe".to_string());
-   ```
-
 ## Example Output
 
 ```shell
-Process started: Notepad.exe (PID: 1234)
-Process stopped: Notepad.exe (PID: 1234)
+Process started: paraview.exe (PID: 1234)
+Process stopped: paraview.exe (PID: 1234)
+Process started: Code.exe (PID: 5678)
 Current process info:
-PID: 1234, Name: Notepad.exe, Start Time: 2024-06-21T12:34:56Z, Stop Time: 2024-06-21T12:35:10Z
+PID: 1234, Name: paraview.exe, Start Time: 2024-06-21T12:34:56Z, Stop Time: 2024-06-21T12:35:10Z
+PID: 5678, Name: Code.exe, Start Time: 2024-06-21T12:40:00Z, Stop Time: N/A
 ```
 
 ## License
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,36 @@
-use chrono::{DateTime, Utc};
+use chrono::Duration as ChronoDuration;
+use chrono::{DateTime, TimeZone, Utc};
 use serde::Deserialize;
+use serde_yaml::from_reader;
 use std::collections::HashMap;
 use std::error::Error;
+use std::fs::File;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 use wmi::{COMLibrary, FilterValue, WMIConnection};
 
 #[derive(Deserialize, Debug)]
+struct Config {
+    watch_list: Vec<String>,
+}
+
+#[derive(Deserialize, Debug)]
 #[serde(rename = "Win32_ProcessStartTrace")]
-#[serde(rename_all = "PascalCase")]
+#[allow(non_snake_case)]
 struct ProcessStartTrace {
-    process_id: u32,
-    process_name: String,
+    ProcessID: u32,
+    ProcessName: String,
+    TIME_CREATED: u64,
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename = "Win32_ProcessStopTrace")]
-#[serde(rename_all = "PascalCase")]
+#[allow(non_snake_case)]
 struct ProcessStopTrace {
-    process_id: u32,
-    process_name: String,
+    ProcessID: u32,
+    ProcessName: String,
+    TIME_CREATED: u64,
 }
 
 #[derive(Debug)]
@@ -31,22 +41,56 @@ struct ProcessInfo {
 }
 
 fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
-    let process_info: Arc<Mutex<HashMap<u32, ProcessInfo>>> = Arc::new(Mutex::new(HashMap::new()));
-    let process_name = Arc::new("Notepad.exe".to_string());
+    let file = File::open("config.yaml")?;
+    let config: Config = from_reader(file)?;
 
-    let start_handle = spawn_start_thread(Arc::clone(&process_info), Arc::clone(&process_name));
-    let stop_handle = spawn_stop_thread(Arc::clone(&process_info), Arc::clone(&process_name));
-    let print_handle = spawn_print_thread(Arc::clone(&process_info));
+    let map_process_info: Arc<Mutex<HashMap<u32, ProcessInfo>>> =
+        Arc::new(Mutex::new(HashMap::new()));
+    let watch_list = Arc::new(config.watch_list);
 
-    start_handle.join().unwrap()?;
-    stop_handle.join().unwrap()?;
+    let start_handles: Vec<_> = watch_list
+        .iter()
+        .map(|process_name| {
+            spawn_start_thread(
+                Arc::clone(&map_process_info),
+                Arc::new(process_name.clone()),
+            )
+        })
+        .collect();
+
+    let stop_handles: Vec<_> = watch_list
+        .iter()
+        .map(|process_name| {
+            spawn_stop_thread(
+                Arc::clone(&map_process_info),
+                Arc::new(process_name.clone()),
+            )
+        })
+        .collect();
+    let print_handle = spawn_print_thread(Arc::clone(&map_process_info));
+
+    for handle in start_handles {
+        handle.join().unwrap()?;
+    }
+    for handle in stop_handles {
+        handle.join().unwrap()?;
+    }
     print_handle.join().unwrap();
 
     Ok(())
 }
 
+fn convert_wmi_time_to_utc(time_created: u64) -> DateTime<Utc> {
+    let seconds = (time_created / 10_000_000) as i64;
+    let nanoseconds = ((time_created % 10_000_000) * 100) as u32;
+    let base_time = Utc.with_ymd_and_hms(1601, 1, 1, 0, 0, 0).unwrap();
+    let duration =
+        ChronoDuration::seconds(seconds) + ChronoDuration::nanoseconds(nanoseconds as i64);
+    base_time + duration
+}
+
 fn spawn_start_thread(
-    process_info: Arc<Mutex<HashMap<u32, ProcessInfo>>>,
+    map_process_info: Arc<Mutex<HashMap<u32, ProcessInfo>>>,
     process_name: Arc<String>,
 ) -> thread::JoinHandle<Result<(), Box<dyn Error + Send + Sync>>> {
     thread::spawn(move || {
@@ -60,7 +104,7 @@ fn spawn_start_thread(
         let wmi_con = WMIConnection::new(com_con)?;
         let start_iterator = wmi_con.filtered_notification::<ProcessStartTrace>(
             &start_filters,
-            Some(Duration::from_secs(1)),
+            Some(Duration::from_millis(500)),
         )?;
 
         for result in start_iterator {
@@ -68,14 +112,14 @@ fn spawn_start_thread(
                 Ok(start_trace) => {
                     println!(
                         "Process started: {} (PID: {})",
-                        start_trace.process_name, start_trace.process_id
+                        start_trace.ProcessName, start_trace.ProcessID
                     );
-                    let mut info = process_info.lock().unwrap();
-                    info.insert(
-                        start_trace.process_id,
+                    let mut map = map_process_info.lock().unwrap();
+                    map.insert(
+                        start_trace.ProcessID,
                         ProcessInfo {
-                            process_name: start_trace.process_name,
-                            start_time: Some(Utc::now()),
+                            process_name: start_trace.ProcessName,
+                            start_time: Some(convert_wmi_time_to_utc(start_trace.TIME_CREATED)),
                             stop_time: None,
                         },
                     );
@@ -91,7 +135,7 @@ fn spawn_start_thread(
 }
 
 fn spawn_stop_thread(
-    process_info: Arc<Mutex<HashMap<u32, ProcessInfo>>>,
+    map_process_info: Arc<Mutex<HashMap<u32, ProcessInfo>>>,
     process_name: Arc<String>,
 ) -> thread::JoinHandle<Result<(), Box<dyn Error + Send + Sync>>> {
     thread::spawn(move || {
@@ -105,7 +149,7 @@ fn spawn_stop_thread(
         let wmi_con = WMIConnection::new(com_con)?;
         let stop_iterator = wmi_con.filtered_notification::<ProcessStopTrace>(
             &stop_filters,
-            Some(Duration::from_secs(1)),
+            Some(Duration::from_millis(500)),
         )?;
 
         for result in stop_iterator {
@@ -113,11 +157,12 @@ fn spawn_stop_thread(
                 Ok(stop_trace) => {
                     println!(
                         "Process stopped: {} (PID: {})",
-                        stop_trace.process_name, stop_trace.process_id
+                        stop_trace.ProcessName, stop_trace.ProcessID
                     );
-                    let mut info = process_info.lock().unwrap();
-                    if let Some(process_info) = info.get_mut(&stop_trace.process_id) {
-                        process_info.stop_time = Some(Utc::now());
+                    let mut map = map_process_info.lock().unwrap();
+                    if let Some(map_process_info) = map.get_mut(&stop_trace.ProcessID) {
+                        map_process_info.stop_time =
+                            Some(convert_wmi_time_to_utc(stop_trace.TIME_CREATED));
                     }
                 }
                 Err(err) => {
@@ -131,22 +176,22 @@ fn spawn_stop_thread(
 }
 
 fn spawn_print_thread(
-    process_info: Arc<Mutex<HashMap<u32, ProcessInfo>>>,
+    map_process_info: Arc<Mutex<HashMap<u32, ProcessInfo>>>,
 ) -> thread::JoinHandle<()> {
     thread::spawn(move || loop {
         thread::sleep(Duration::from_secs(5));
-        let info = process_info.lock().unwrap();
+        let info = map_process_info.lock().unwrap();
         println!("Current process info:");
-        for (pid, process_info) in info.iter() {
+        for (pid, map_process_info) in info.iter() {
             println!(
                 "PID: {}, Name: {}, Start Time: {:}, Stop Time: {:}",
                 pid,
-                process_info.process_name,
-                process_info
+                map_process_info.process_name,
+                map_process_info
                     .start_time
                     .map_or("N/A".to_string(), |dt| dt.to_string()),
-                process_info
-                    .start_time
+                map_process_info
+                    .stop_time
                     .map_or("N/A".to_string(), |dt| dt.to_string()),
             );
         }
